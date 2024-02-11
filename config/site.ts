9ef6da0da232680a02c4e604b2ea9ba0eba7d5c4
BoredApe8461 <letesinkelegesse@gmail.com>
2024-02-11 16:36:29 +0100

    fix

--- config/site.ts
+++ config/site.ts
@@ -3,27 +3,10 @@ import { SubstrateChain, development } from "@scio-labs/use-inkathon"
 
 export type SiteConfig = typeof siteConfig
 
-const polkadotRelay: SubstrateChain = {
-  network: "Polkadot",
-  name: "Polkadot Relay Chain",
-  rpcUrls: ["wss://rpc.polkadot.io"],
-  ss58Prefix: 0,
-  testnet: false,
-}
-
-const rococoTestnet: SubstrateChain = {
-  network: "Rococo",
-  name: "Rococo Testnet",
-  rpcUrls: ["wss://rococo-rpc.polkadot.io"],
-  ss58Prefix: 42,
-  testnet: true,
-}
-
 export const siteConfig = {
   name: "Polkadot Weigher",
   description: "Displays utilization of Polkadot parachains.",
   backendUrl: "https://api.polkadot-weigher.com",
-  defaultChain: rococoTestnet || development,
   blockExplorer: "https://rococo.subscan.io/extrinsic/",
   subscriptionCost: "1000000000", // cost to register a parachain
   mainNav: [
